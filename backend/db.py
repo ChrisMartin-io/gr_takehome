5384c51c7ea80a88cf6d1d97a10f5bcce3e07dc0
Chris <chris@Chriss-Mac-mini.local>
2021-10-04 08:11:09 -0700

    main commit

--- backend/db.py
+++ backend/db.py
@@ -5,12 +5,11 @@ import pandas as pd
 # populate data from csv using pandas
 # ensure primary key active on books table
 def books_init(db):
-
     engine = db.get_engine()
     csv_file_path = 'books2.csv'
     con = engine.connect()
 
-    con.execute('DROP TABLE IF EXISTS borrowed_books, books, users')
+    con.execute('DROP TABLE IF EXISTS borrowed_books, books, users, bborrowed')
     with open(csv_file_path, 'r') as file:
         csv_data = pd.read_csv(file)
     csv_data.to_sql('books',
@@ -22,13 +21,12 @@ def books_init(db):
     con.execute('ALTER TABLE books ADD PRIMARY KEY (id);')
     db.create_all()
 
-# init users table
+# init users table, add users
 def users_init(db, app):
-
     from models import User
     with app.app_context():
-        db.session.add(User(name='Ada'))
-        db.session.add(User(name='Zeke'))
-        db.session.add(User(name='Fia'))
-        db.session.add(User(name='Bob'))
+        db.session.add(User(name='Ada', id=0))
+        db.session.add(User(name='Zeke', id=1))
+        db.session.add(User(name='Fia', id=2))
+        db.session.add(User(name='Bob', id=3))
         db.session.commit()
--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKeyConstraint, UniqueConstraint
+from sqlalchemy.orm import relationship
 from db import books_init, users_init
 from dataclasses import dataclass
 
@@ -11,7 +12,6 @@ def connect_db(app):
     db.init_app(app)
     books_init(db)
     users_init(db, app)
-
 
 # classes: Book, User, BorrowedBooks
 @dataclass
@@ -26,6 +26,10 @@ class Book(db.Model):
     genre: str
 
     __tablename__ = "books"
+    def __getitem__(self, title):
+        return self.__dict__[title]
+    def __getitem__(self, id):
+        return self.__dict__[id]
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.Text, unique=True)
@@ -35,6 +39,7 @@ class Book(db.Model):
     price = db.Column(db.Integer)
     year = db.Column(db.Integer)
     genre = db.Column(db.Text)
+    users = relationship('User', secondary = 'borrowed_books')
 
 @dataclass
 class User(db.Model):
@@ -42,9 +47,13 @@ class User(db.Model):
     name: str
 
     __tablename__ = "users"
+    def __getitem__(self, name):
+        return self.__dict__[name]
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String())
+    books = relationship('Book', secondary = 'borrowed_books', overlaps='users')
+
 
 @dataclass
 class Borrow(db.Model):
@@ -54,18 +63,21 @@ class Borrow(db.Model):
     __tablename__ = "borrowed_books"
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), primary_key=True)
+    book_id = db.Column(db.Integer,
+                        db.ForeignKey('books.id'),
+                        primary_key=True)
 
-    # add record
+    # add borrowed book
     def add(user, book):
-      new_obj = Borrow(user_id=user, book_id=book)
-      db.session.add(new_obj)
-      db.session.commit()
-      return Borrow.query.get(book)
+        new_obj = Borrow(user_id=user, book_id=book)
+        db.session.add(new_obj)
+        db.session.commit()
+        return Borrow.query.get(book)
 
-
-    # remove record
+    # remove borrowed book
     def remove(book):
-      print('book is', book)
-      Borrow.query.filter('book_id' == book.book_id).delete()
-      return True
+        new_obj = Borrow.query.filter_by(book_id=book.book_id).first()
+        db.session.delete(new_obj)
+        db.session.commit()
+        return True
+
